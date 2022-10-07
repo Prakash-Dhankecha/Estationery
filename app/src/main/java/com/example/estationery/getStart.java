a87ca6ddddee1c5d82f47b2124299d6453a10e6d
Darshan Zalavadiya <dzalavadiya376@rku.ac.in>
2022-10-07 10:17:27 +0530

    commit

--- app/src/main/java/com/example/estationery/getStart.java
+++ app/src/main/java/com/example/estationery/getStart.java
@@ -10,5 +10,6 @@ public class getStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_start);
+        //darsgabnm
     }
 }
