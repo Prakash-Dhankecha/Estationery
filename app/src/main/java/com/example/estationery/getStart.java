518ab1f4459a0d79f68570467a19cd55c6e1117a
Darshan Zalavadiya <dzalavadiya376@rku.ac.in>
2022-10-07 11:00:37 +0530

    commit

--- app/src/main/java/com/example/estationery/getStart.java
+++ app/src/main/java/com/example/estationery/getStart.java
@@ -2,14 +2,26 @@ package com.example.estationery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class getStart extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_start);
-        //darsgabnm
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getStart.this,LoginScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
