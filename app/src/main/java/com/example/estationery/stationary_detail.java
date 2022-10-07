0d5a1d3581508494ac6cd34d0bb9a1b85fb1565f
Shah Nawaz <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>
2022-10-08 03:08:21 +0400

    commit

---
+++ app/src/main/java/com/example/estationery/stationary_detail.java
@@ -0,0 +1,14 @@
+package com.example.estationery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stationary_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stationary_detail);
+    }
+}
