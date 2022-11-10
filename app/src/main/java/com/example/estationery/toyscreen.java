5aa2d32f391867808433ebf2e1e58e35c6a03072
Shah Nawaz <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>
2022-11-10 20:05:07 +0400

    committ

---
+++ app/src/main/java/com/example/estationery/toyscreen.java
@@ -0,0 +1,14 @@
+package com.example.estationery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class toyscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toyscreen);
+    }
+}
